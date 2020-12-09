bba978612e6a9d2de13936204ea6a4f005db99f5
Gonzalo Scarlata <gonza.sc.gs@gmail.com>
2020-12-09 17:57:37 -0300

    exterior de cards y tamaño de secction stock listo

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-const cardShowed = document.getElementById('cardShowed');
-const products = JSON.parse(localStorage.getItem('products')) || [];
+const cardShowed = document.getElementById("cardShowed");
+const products = JSON.parse(localStorage.getItem("products")) || [];
 
 function createProduct() {
-    //Traer los productos de local storage
-    const products = JSON.parse(localStorage.getItem('products')) || [];
-    const cardsProducts = [];
-    
-    for (let i = 0; i < products.length; i++) {
-        const product = products[i];
-        const card = `
-        <div class="card">
-            <img class="imgProduct" src="${product.productImg}" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${product.productName}</h5>
-                    <p class="card-text">${product.productDescription}</p>
+  //Traer los productos de local storage
+  const products = JSON.parse(localStorage.getItem("products")) || [];
+  const cardsProducts = [];
+
+  for (let i = 0; i < products.length; i++) {
+    const product = products[i];
+    const card = `
+        <div class="card text-center" style="width: 18rem;">
+            <div class="text-center">
+                <img src="${product.productImg}" class="card-img-top" style="width: 18rem; height: 190px;" alt="imágen del producto">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">${product.productName}</h5>
+                <p class="card-text">${product.productDescription}</p>
+                <div class="card-text text-center">
+                    <a href="#" class="btn btn-primary">Agregar al carrito</a>
                 </div>
+            </div>
         </div>
-        `
+        `;
 
-
-        cardsProducts.unshift(card);
-        
-    }
-    cardShowed.innerHTML = cardsProducts.join('');
+    cardsProducts.unshift(card);
+  }
+  cardShowed.innerHTML = cardsProducts.join("");
 }
 createProduct();

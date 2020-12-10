38484cd4e6ae3ae127200665e328a18d5810c71d
Gonzalo Scarlata <gonza.sc.gs@gmail.com>
2020-12-10 20:40:43 -0300

    carrito listo al localestorage

--- index.js
+++ index.js
@@ -9,23 +9,22 @@ function createProduct() {
   for (let i = 0; i < products.length; i++) {
     const product = products[i];
     const card = `
-        <div class="card text-center mb-2 p-2 d-flex align-content-around flex-wrap" style="width: 18rem;">
+        <div class="card text-center mb-2 p-2 d-flex align-content-between flex-wrap" style="width: 18rem;">
             <div class="text-center">
                 <img src="${product.productImg}" class="card-img-top" style="width: 18rem; height: 190px;" alt="imágen del producto">
             </div>
             <div class="card-body ">
                 <h5 class="card-title">${product.productName}</h5>
-                <p class="card-text">${product.productDescription}</p>
+                <p class="card-text">Precio: $${product.productPrice}</p>
             </div>
             <div>
                 <div class="card-text text-center">
-                    <button type="button" id="${product.id}" class="btn btn-primary" onclick="addProductToCart(${product.id})">Agregar al carrito</button>
+                    <button type="button" id="${product.id}" class="btn btn-primary" onclick="addProductToCart('${product.id}')">Agregar al carrito</button>
                     
                 </div>
             </div>
         </div>
         `;
-        console.log("🚀 ~ file: index.js ~ line 23 ~ createProduct ~ product.id", product.id)
     cardsProducts.unshift(card);
   }
   cardShowed.innerHTML = cardsProducts.join("");
@@ -33,11 +32,17 @@ function createProduct() {
 createProduct();
 
 function addProductToCart(Id) {
+    const cartProducts = JSON.parse(localStorage.getItem('cartProducts')) || [];
+    console.log("🚀 ~ file: index.js ~ line 36 ~ addProductToCart ~ cartProducts", cartProducts)
+    //console.log("🚀 ~ file: index.js ~ line 36 ~ addProductToCart ~ cartProducts", cartProducts)
     const products = JSON.parse(localStorage.getItem('products')) || [];
-    console.log("🚀 ~ file: index.js ~ line 37 ~ addProductToCart ~ products", products)
-    const product = products.filter((element) => element.id === Id);
-    console.log("🚀 ~ file: index.js ~ line 38 ~ addProductToCart ~ product", product)
-    const productsJson = JSON.stringify(product);
-    localStorage.setItem('cartProducts', productsJson);
-  
+    const cartProduct = products.find(element => element.id === Id);
+    //console.log("🚀 ~ file: index.js ~ line 39 ~ addProductToCart ~ cartProduct", cartProduct)
+
+    cartProducts.push(cartProduct);
+    console.log("🚀 ~ file: index.js ~ line 42 ~ addProductToCart ~ cartProducts", cartProducts)
+    
+    
+    const cartProductJson = JSON.stringify(cartProducts);
+    localStorage.setItem('cartProducts', cartProductJson);
 }
